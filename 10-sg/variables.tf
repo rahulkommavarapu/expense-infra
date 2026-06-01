6237657a0b0cb449f5692a924fb4e05aa9427fcb
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-06-01 13:31:48 +0530

    Expense

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -14,3 +14,34 @@ variable "common_tags" {
     terraform   = "true"
   }
 }
+variable "vpc_tags" {
+  default = {
+    purpose = "assignment"
+  }
+}
+variable "sg_tags" {
+  default = {
+    Name = "mysql-sg"
+  }
+}
+
+variable "db_password" {
+  sensitive = true
+}
+
+
+
+variable "is_peering_required" {
+  default = false
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "private_subnet_cidrs" {
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+variable "database_subnet_cidrs" {
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
