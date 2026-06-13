efa3a0436694a5e821a8e408dccf58774671758b
rahulkommavarapu <167281463+rahulkommavarapu@users.noreply.github.com>
2026-06-13 16:51:22 +0530

    Delete parameter..tf

--- parameter..tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "ingress_alb_certificate_arn" {
-  name  = "/${var.project_name}/${var.environment}/ingress_alb_certificate_arn"
-  type  = "String"
-  value = aws_acm_certificate.expense.arn
-}
