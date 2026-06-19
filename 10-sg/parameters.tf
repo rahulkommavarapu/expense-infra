ca2e38a152c202cf82e8918aa3ac199512c18584
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-06-19 18:25:20 +0530

    Test

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -40,13 +40,13 @@ resource "aws_ssm_parameter" "vpn_sg_id" {
 resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
   name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
   type = "String"
-  value = module.eks_control_plane_sg
+  value = module.eks_control_plane_sg.sg_id
 }
 
 resource "aws_ssm_parameter" "eks_node_sg_id" {
   name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
   type = "String"
-  value = module.eks_node_sg_id  
+  value = module.eks_node_sg_id.sg_id  
 }
 
 
