026bf444d0af63ca2798fadcdfb8b1b29b488793
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-06-19 15:44:14 +0530

    Test

--- 40-eks/provider.tf
+++ 40-eks/provider.tf
@@ -5,11 +5,12 @@ terraform {
       version = "5.95.0"
     }
   }
+  
   backend "s3" {
-    bucket         = "83s-remote-state-devlop"
-    key            = "expense-dev-eks"
+    bucket         = "rahul-remote-state"
+    key            = "expense-dev-vpc"
     region         = "us-east-1"
-    dynamodb_table = "83s-remote-state-devlop"
+    dynamodb_table = "83s-remote-state-env"
   }
 
 
