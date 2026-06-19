19f7b16509c9f8322bf505d5bb27d53fdcb493c3
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-06-19 18:28:41 +0530

    Test

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -44,9 +44,9 @@ resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
 }
 
 resource "aws_ssm_parameter" "eks_node_sg_id" {
-  name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
-  type = "String"
-  value = module.eks_node_sg_id.sg_id  
+  name  = "/${var.project_name}/${var.environment}/eks_node_sg_id"
+  type  = "String"
+  value = module.eks_node_sg.sg_id
 }
 
 
