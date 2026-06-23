ee54d1998e48a9336c90d2df25b9afd853ea728e
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-06-23 20:59:12 +0530

    Test

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -22,13 +22,13 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
   value = module.bastion_sg.sg_id
 }
 
-# For Load Balancer Purpose we Create SSM Parameter
-resource "aws_ssm_parameter" "app_alb_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
-  type  = "String"
-  value = module.app_alb_sg.sg_id
+# # For Load Balancer Purpose we Create SSM Parameter
+# resource "aws_ssm_parameter" "app_alb_sg_id" {
+#   name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+#   type  = "String"
+#   value = module.app_alb_sg.sg_id
 
-}
+# }
 #  Create the SSM Parameter For VPN_SG
 # So WE Can Export the VPN_SG
 resource "aws_ssm_parameter" "vpn_sg_id" {
